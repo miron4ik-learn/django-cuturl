7a2f664ebf635d0a1d76e674c6ae790120e4695e
miron4ik <miron4ik.life@yandex.ru>
2022-09-08 07:17:58 +0300

    Create db models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Link
 
-# Register your models here.
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+  list_display = [ 'long_url_cut', 'short_url', 'views' ]
+  
+  def long_url_cut(self, obj):
+    return obj.long_url[0:50]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Link(models.Model):
+  class Meta:
+    verbose_name = 'ссылку'
+    verbose_name_plural = 'ссылки'
+    
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  long_url = models.URLField(verbose_name='Длинная ссылка')
+  short_url = models.CharField(verbose_name='Короткая ссылка', max_length=20)
+  views = models.PositiveIntegerField(verbose_name='Переходы', default=0)
