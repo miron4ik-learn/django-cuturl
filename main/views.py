67f0285f766b6a5dbf65a519c027a7e39d65466d
miron4ik <miron4ik.life@yandex.ru>
2022-09-08 08:01:34 +0300

    Display info on the home page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from django.contrib.auth.models import User
+from .models import Link
 
 def index(request):
-  return render(request, 'main/index.html')
+  user_count = User.objects.all().count()
+  link_count = Link.objects.all().count()
+  views_count = Link.objects.aggregate(Sum('views'))['views__sum']
+  
+  return render(request, 'main/index.html', {
+    'user_count': user_count,
+    'link_count': link_count,
+    'views_count': views_count,
+  })
